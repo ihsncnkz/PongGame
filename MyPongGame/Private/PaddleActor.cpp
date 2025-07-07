449fb0de6fa256e04cada3796a8c58d536597947
ihsncnkz <ihsncnkz@gmail.com>
2025-07-07 21:02:23 +0300

    v0.1

---
+++ MyPongGame/Private/PaddleActor.cpp
@@ -0,0 +1,29 @@
+
+
+
+#include "PaddleActor.h"
+
+
+// Sets default values
+APaddleActor::APaddleActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APaddleActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APaddleActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
